b46f8498ab78060205d89740eb86f5399267f6a6
Elkasitu <adt@odoo.com>
2017-05-21 13:03:07 +0200

    Change the way accordion pannel ids are generated so that it uses an enumerated list. Change the way path to video files are obtained: hardcoded -> programmatically

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from flask import request, Flask, render_template
 from os import walk
 
 app = Flask(__name__)
+SRC_PATH = 'static/content/'
 
 @app.route('/')
 def index():
     files = []
     # walk the content folder recursively and add all
     # filenames to the files list
-    for (dirpath, dirnames, filenames) in walk('static/content/'):
+    for (dirpath, dirnames, filenames) in walk(SRC_PATH):
         files.extend(filenames)
-    return render_template('index.html', files=files)
+
+    enum = enumerate(files)
+
+    return render_template('index.html', files=enum, path=SRC_PATH)
