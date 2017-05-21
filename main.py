80103d2d7f45cf99d7543f1933292782c8ab59b6
Elkasitu <adt@odoo.com>
2017-05-21 15:10:11 +0200

    Create a new route for index and one for shows/<showname>, add new index.html and functionality for show.html in the show_tv controller

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from flask import request, Flask, render_template
-from os import walk
+from os import walk, listdir
 
 app = Flask(__name__)
-SRC_PATH = 'static/content/'
+SRC_PATH = 'static/'
 
 @app.route('/')
 def index():
+    shows_path = SRC_PATH + 'content/'
+    thumb_path = SRC_PATH + 'thumbnails/'
+    folders = listdir(shows_path)
+    return render_template('index.html', shows=folders, path=thumb_path)
+
+@app.route('/shows/<showname>')
+def tv_show(showname):
+    path = SRC_PATH + 'content/' +  showname + '/'
     files = []
     # walk the content folder recursively and add all
     # filenames to the files list
-    for (dirpath, dirnames, filenames) in walk(SRC_PATH):
+    for (dirpath, dirnames, filenames) in walk(path):
         files.extend(filenames)
 
-    enum = enumerate(files)
+    print(files)
 
-    return render_template('index.html', files=enum, path=SRC_PATH)
+    enum = enumerate(files)
+    return render_template('show.html', files=enum, path=path)
