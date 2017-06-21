03d3d78312ab60a063eb65b97bbc423f48c9c555
Elkasitu <adt@odoo.com>
2017-06-21 16:59:45 +0200

    Move old index to /shows endpoint
    Create a proper home page
    Handle current location in the navbar
    Format show names

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-from flask import request, Flask, render_template
+from flask import Flask, render_template
 from os import walk, listdir
 
 app = Flask(__name__)
 SRC_PATH = 'static/'
 
+
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/shows')
+def tv_shows():
     shows_path = SRC_PATH + 'content/'
     thumb_path = SRC_PATH + 'thumbnails/'
     folders = listdir(shows_path)
-    return render_template('index.html', shows=folders, path=thumb_path)
+    return render_template('shows.html', shows=folders, path=thumb_path)
+
 
 @app.route('/shows/<showname>')
 def tv_show(showname):
-    path = SRC_PATH + 'content/' +  showname + '/'
+    path = SRC_PATH + 'content/' + showname + '/'
     files = []
     # walk the content folder recursively and add all
     # filenames to the files list
     for (dirpath, dirnames, filenames) in walk(path):
         files.extend(filenames)
 
-    print(files)
-
     enum = enumerate(files)
     return render_template('show.html', files=enum, path=path)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
